c946ba0e6e25649667da550973e80b42d30ea331
AndroidParanoid <90028731+AndroidParanoi@users.noreply.github.com>
2021-09-03 12:03:47 +0100

    Update sheep.go

--- sheep.go
+++ sheep.go
@@ -27,41 +27,38 @@ func OrganizeInputTags(url string, wg *sync.WaitGroup, sem chan bool) {
 			return
 		} else {
 			forms := doc.Find("form")
-			if forms.Size() > 0 {
-				var complete_input_tags_name string
-				forms.Each(func(_ int, selection *goquery.Selection) {
-					method, _ := selection.Attr("method")
-					if method != "" {
-						method = strings.ToLower(method)
-						if method == "get" {
-							input_tags := selection.Find("input")
-							input_tags.Each(func(_ int, s *goquery.Selection) {
-								input_tags_name, _ := s.Attr("name")
-								if input_tags_name != "" {
-									complete_input_tags_name += "," + input_tags_name + "=1"
-								} else {
-									return
-								}
-							})
-							if complete_input_tags_name != "" {
-								if strings.Contains(url, "?") {
-									complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "&", -1)
-								} else {
-									complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "?", 1)
-									complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "&", -1)
-								}
-								new_url := fmt.Sprintf("%s%s", url, complete_input_tags_name)
-								fmt.Println(new_url)
+			var complete_input_tags_name string
+			forms.Each(func(_ int, selection *goquery.Selection) {
+			method, _ := selection.Attr("method")
+			if method != "" {
+				method = strings.ToLower(method)
+				if method == "get" {
+					input_tags := selection.Find("input")
+					input_tags.Each(func(_ int, s *goquery.Selection) {
+						input_tags_name, _ := s.Attr("name")
+						if input_tags_name != "" {
+							complete_input_tags_name += "," + input_tags_name + "=1"
+						} else {
+							return
+						}
+					})
+						if complete_input_tags_name != "" {
+							if strings.Contains(url, "?") {
+								complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "&", -1)
+							} else {
+								complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "?", 1)
+								complete_input_tags_name = strings.Replace(complete_input_tags_name, ",", "&", -1)
 							}
+							new_url := fmt.Sprintf("%s%s", url, complete_input_tags_name)
+							fmt.Println(new_url)
 						}
 					}
-				})
-			} else {
-				return
-			}
+				}
+			})
 		}
 	}
 }
+
 func main() {
 	var wg sync.WaitGroup
 	conc := flag.Int("concurrency", 10, "concurrency level")
